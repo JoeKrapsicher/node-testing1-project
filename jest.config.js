086cfde9d32244830dacae49d6c631a7b4cba7d0
JoeKrapsicher <JoeKrapsicher@gmail.com>
2021-09-13 19:28:32 -0600

    Completed Assignment

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/private/var/folders/78/wsw4bc497l1b0dlb3f77xh380000gn/T/jest_dx",
+  // cacheDirectory: "/tmp/jest_rs",
 
   // Automatically clear mock calls and instances between every test
   // clearMocks: false,
